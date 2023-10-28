e60bc39165b3e9e3775cbc663f6fea6828428bb3
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2023-10-28 16:04:18 +0300

    Update-01
    add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+//random color
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+//set link on button by data-atribute
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+//default value of button-stop
+btnStop.setAttribute('disabled', 'false');
+
+btnStart.addEventListener('click', clickStart);
+btnStop.addEventListener('click', clickStop);
+
+//add timer
+let timerId = null;
+
+function clickStart(event) {
+  btnStop.removeAttribute('disabled');
+  btnStart.setAttribute('disabled', 'false');
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function clickStop(event) {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', 'false');
+  clearInterval(timerId);
+}
