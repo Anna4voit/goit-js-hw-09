e0ee99b2118b5dc838946b42fa7480cef81eea88
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2023-10-31 17:11:59 +0000

    Deploying to gh-pages from @ Anna4voit/goit-js-hw-09@a9467e117fcd645044d278a6474b8097e0a59486 🚀

--- 01-color-switcher.406ce132.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled","false"),t.addEventListener("click",(function(d){e.removeAttribute("disabled"),t.setAttribute("disabled","false"),a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(d){t.removeAttribute("disabled"),e.setAttribute("disabled","false"),clearInterval(a)}));let a=null;
-//# sourceMappingURL=01-color-switcher.406ce132.js.map
